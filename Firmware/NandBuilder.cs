01e07f47b3d4f6435e35219fc002d393cdd465e1
elise <elisezerotwo@gmail.com>
2020-04-28 23:10:47 +0200

    Add NandBuilder class

---
+++ Firmware/NandBuilder.cs
@@ -0,0 +1,21 @@
+namespace nxfw_tool.Firmware
+{
+    public class NandBuilder
+    {
+        public string FirmwareDirectory;
+        public string OutputDir;
+        public NandBuilder(string firmwareDir, string outputDir)
+        {
+            FirmwareDirectory = firmwareDir;
+            OutputDir = outputDir;
+        }
+
+        public int Build()
+        {
+            int res = 0;  
+
+            retn:
+            return res;
+        }
+    }
+}
