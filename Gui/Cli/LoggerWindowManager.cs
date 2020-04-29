4c03b7c8dbbc33848ddefaed356536955554de38
elise <elisezerotwo@gmail.com>
2020-04-29 12:48:32 +0200

    Add LoggerWindowManager class

---
+++ Gui/Cli/LoggerWindowManager.cs
@@ -0,0 +1,19 @@
+using Terminal.Gui;
+namespace nxfw_tool.Gui.Cli
+{
+    public class LoggerWindowManager
+    {
+        public Window ChildWindow;
+
+        public void Log(string message)
+        {
+            
+        }
+
+        public LoggerWindowManager(Window window)
+        {
+            ChildWindow = window;
+        }
+        
+    }
+}
